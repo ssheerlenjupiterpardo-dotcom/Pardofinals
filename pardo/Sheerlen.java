6319d74d149dbe10d44741647abb27b6cc0e5a8a
ssheerlenjupiterpardo-dotcom <ssheerlenjupiterpardo@gmail.com>
2025-12-01 09:23:54 +0800

    Add files via upload
    December 1,2025 Initial code

---
+++ pardo/Sheerlen.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Sheerlen here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Sheerlen
+{
+   public static void main(String [] args){ 
+   Scanner input = new Scanner (System.in);
+   
+    while(true){
+       System.out.print("Enter between (1-100): ");
+    int userInput=input.nextInt();
+    if(userInput>=1 && userInput<=100){
+      break;
+      
+    }
+ 
+
+}
+
+}
+
+}
+    
+      
+
+    
+
